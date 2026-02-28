e54001f9377964a7bbbadfe810997281bbfffd7a
MaxK0 <maxkoryakov1@gmail.com>
2026-02-28 09:55:00 +0500

    burger menu

--- public/js/app.js
+++ public/js/app.js
@@ -23,6 +23,69 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 
+    // Получаем элементы
+    const burgerMenu = document.getElementById('header__menu');
+    const headerUl = document.querySelector('.header__ul');
+
+    // Создаем элемент overlay для затемнения фона
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+
+    // Обработчик клика на бургер-меню
+    burgerMenu.addEventListener('click', function() {
+        // Переключаем классы active
+        burgerMenu.classList.toggle('active');
+        headerUl.classList.toggle('active');
+        overlay.classList.toggle('active');
+
+        // Блокируем прокрутку страницы при открытом меню
+        if (headerUl.classList.contains('active')) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+    });
+
+    // Обработчик клика на overlay
+    overlay.addEventListener('click', function() {
+        // Убираем классы active
+        burgerMenu.classList.remove('active');
+        headerUl.classList.remove('active');
+        overlay.classList.remove('active');
+
+        // Возвращаем прокрутку страницы
+        document.body.style.overflow = '';
+    });
+
+    // Закрываем меню при клике на пункт меню
+    const menuLinks = document.querySelectorAll('.header__ul a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            // Убираем классы active
+            burgerMenu.classList.remove('active');
+            headerUl.classList.remove('active');
+            overlay.classList.remove('active');
+
+            // Возвращаем прокрутку страницы
+            document.body.style.overflow = '';
+        });
+    });
+
+    // Закрываем меню при изменении размера окна
+    window.addEventListener('resize', function() {
+        // Если ширина экрана больше 768px и меню открыто, закрываем его
+        if (window.innerWidth > 480 && headerUl.classList.contains('active')) {
+            burgerMenu.classList.remove('active');
+            headerUl.classList.remove('active');
+            overlay.classList.remove('active');
+
+            // Возвращаем прокрутку страницы
+            document.body.style.overflow = '';
+        }
+    });
+
+
     const phoneInputs = document.querySelectorAll('input[type="tel"]');
 
     phoneInputs.forEach(input => {
